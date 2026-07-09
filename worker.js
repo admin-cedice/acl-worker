527ddca314edba2319d3938949916ba06f143a4a
admin-cedice <admin@liberalmente.app>
2026-07-09 15:58:48 -0400

    Add files via upload

--- worker.js
+++ worker.js
@@ -49,7 +49,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://liberalmente.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-worker-secret, x-auditoria-id');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-worker-secret, x-auditoria-id, x-titulo, x-autor, x-descripcion, x-tipo-archivo');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
